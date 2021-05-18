0e8cc10709fb39161265341e44569e51f76b4a42
Bum <saykkk0222@naver.com>
2021-05-18 20:44:03 +0900

    테스트3

--- app/src/main/java/com/gatheringandyou/gandyoudemo/bulletinboards/FreeBoard.kt
+++ app/src/main/java/com/gatheringandyou/gandyoudemo/bulletinboards/FreeBoard.kt
@@ -8,7 +8,7 @@ import com.gatheringandyou.gandyoudemo.R
 import com.gatheringandyou.gandyoudemo.adapters.BoardAdapter
 import com.gatheringandyou.gandyoudemo.databinding.ActivityFreeBoardBinding
 
-//테스트
+//테스트1
 
 
 class FreeBoard : AppCompatActivity() {
